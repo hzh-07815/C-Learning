49fb711267f93cc07f4758a8b8f4130b17355c4d
hzh-07815 <2744486949@qq.com>
2026-06-19 18:10:50 +0800

    Create 01_结构体定义.c
    新增01_结构体定义代码

---
+++ 01_结构体/01_结构体定义.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+struct Student
+{
+    char name[20];
+    int age;
+    float score;
+};
+
+int main()
+{
+    struct Student s1 = {"张三", 18, 95.5f};
+
+    printf("name = %s\n", s1.name);
+    printf("age = %d\n", s1.age);
+    printf("score = %.1f\n", s1.score);
+
+    return 0;
+}
